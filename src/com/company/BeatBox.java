0e0fc44c2ffbec92a3b5faf2c1874c7cca4f41f1
axpak7 <desima98@gmail.com>
2020-06-26 16:44:55 +0300

    add serialization

--- src/com/company/BeatBox.java
+++ src/com/company/BeatBox.java
@@ -1,20 +1,23 @@
 package com.company;
 
-import javax.sound.midi.*;
-import javax.swing.*;
-import javax.swing.border.Border;
-import java.util.*;
 import java.awt.*;
+import javax.swing.*;
+import java.io.*;
+import javax.sound.midi.*;
+import java.util.*;
 import java.awt.event.*;
+import java.net.*;
+import javax.swing.event.*;
 
 public class BeatBox {
 
+    JFrame theFrame;
     JPanel mainPanel;
     ArrayList<JCheckBox> checkboxList;
+
     Sequencer sequencer;
     Sequence sequence;
     Track track;
-    JFrame theFrame;
 
     String[] instrumentNames = {"Bass Drum", "Closed Hi-Hat", "Open Hi-Hat", "Acoustic Snare", "Crash Cymbal", "Hand Clap", "High Tom", "Hi Bongo", "Maracas", " Whistle", "Low Conga",
             "Cowbell", "Vibraslap", "Low-mid Tom", "High Agogo", "Open Hi Conga"};
@@ -127,18 +130,21 @@ public class BeatBox {
     }
 
     public class MyStartListener implements ActionListener {
+        @Override
         public void actionPerformed(ActionEvent a) {
             buildTrackAndStart();
         }
     }
 
     public class MyStopListener implements ActionListener {
+        @Override
         public void actionPerformed(ActionEvent a) {
             sequencer.stop();
         }
     }
 
     public class MyUpTempoListener implements ActionListener {
+        @Override
         public void actionPerformed(ActionEvent a) {
             float tempoFactor = sequencer.getTempoFactor();
             sequencer.setTempoFactor((float) (tempoFactor * 1.03));
@@ -146,6 +152,7 @@ public class BeatBox {
     }
 
     public class MyDownTempoListener implements ActionListener {
+        @Override
         public void actionPerformed(ActionEvent a) {
             float tempoFactor = sequencer.getTempoFactor();
             sequencer.setTempoFactor((float) (tempoFactor * .97));
@@ -158,7 +165,7 @@ public class BeatBox {
 
             if (key != 0) {
                 track.add(makeEvent(144, 9, key, 100, i));
-                track.add(makeEvent(128, 9, key, 100, i+1));
+                track.add(makeEvent(128, 9, key, 100, i + 1));
             }
         }
     }
@@ -173,5 +180,48 @@ public class BeatBox {
             ex.printStackTrace();
         }
         return event;
+    }
+
+    public class MySendListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            boolean[] checkBoxState = new boolean[256];
+            for (int i = 0; i < 256; i++) {
+                JCheckBox check = (JCheckBox) checkboxList.get(i);
+                if (check.isSelected())
+                    checkBoxState[i] = true;
+            }
+            try {
+                FileOutputStream fileStream = new FileOutputStream(new File("Checkbox.ser"));
+                ObjectOutputStream os = new ObjectOutputStream(fileStream);
+                os.writeObject(checkBoxState);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public class MyReadInListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            boolean[] checkboxState = null;
+            try {
+                FileInputStream fileIn = new FileInputStream(new File("Checkbox.ser"));
+                ObjectInputStream is = new ObjectInputStream(fileIn);
+                checkboxState = (boolean[]) is.readObject();
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+            for (int i = 0; i < 256; i++) {
+                JCheckBox check = (JCheckBox) checkboxList.get(i);
+                if (checkboxState[i]) {
+                    check.setSelected(true);
+                } else {
+                    check.setSelected(false);
+                }
+            }
+            sequencer.stop();
+            buildTrackAndStart();
+        }
     }
 }
